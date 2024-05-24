86b456c0167f178f35e7475eb3350da1caa5e3b8
SurrealGentleman <alexeev1one@mail.ru>
2024-05-24 19:14:33 +0500

    update json

--- SurGu_Calendar/main/create_json.py
+++ SurGu_Calendar/main/create_json.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 
 list_days_of_week = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']
-time_zone = 'Asia/Yekaterinburg:'
 
 
 def create_json(instance):
@@ -26,17 +25,21 @@ def create_json(instance):
 
         repetition = ''
         if str(instance.repetition) in ['Числитель', 'Знаменатель']:
-            repetition = ';INTERVAL=2'
+            repetition = '2'
 
-        json['datetime_start_lesson'] = (time_zone + str(date_start_lesson.strftime('%Y%m%d')) +
+        json['datetime_start_lesson'] = (str(date_start_lesson.strftime('%Y%m%d')) +
                                          'T' + str(instance.time.time_start.strftime('%H%M%S')))
-        json['datetime_end_lesson'] = (time_zone + str(date_start_lesson.strftime('%Y%m%d')) +
+        json['datetime_end_lesson'] = (str(date_start_lesson.strftime('%Y%m%d')) +
                                        'T' + str(instance.time.time_end.strftime('%H%M%S')))
-        json['repetition'] = str(schedule.end_schedule.strftime('%Y%m%d')) + 'T235959' + repetition
+        json['repetition'] = str(schedule.end_schedule.strftime('%Y%m%d')) + 'T235959Z'
+        json['interval'] = repetition
         json['create'] = str(datetime.now().strftime('%Y%m%d'))
         json['location'] = instance.campus.reduction+instance.audience.number_audience+' '+instance.type.name_type
         json['summary'] = instance.discipline.name_discipline
         json['description'] = (f"{instance.professor.last_name} {instance.professor.first_name[0]}."
                                f"{instance.professor.patronymic[0]}.")
-        json['subgroup'] = f"{schedule.subgroup.group.number_group} {schedule.subgroup.name_subgroup}"
+        if schedule.subgroup.name_subgroup:
+            json['subgroup'] = f"{schedule.subgroup.group.number_group}{schedule.subgroup.name_subgroup}"
+        else:
+            json['subgroup'] = f"{schedule.subgroup.group.number_group}"
     return json
