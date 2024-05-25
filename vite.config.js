1a3683abdab1283aca550621d7e45334efe5bf57
DundIR-PC <ruzin-de@yandex.ru>
2024-05-25 09:21:12 +0500

    update settings

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react'
 const backend = 'http://localhost:8000'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	plugins: [react()],
 	server: {
 		host: '0.0.0.0',
 		proxy: {
 			'^/api': {
-				target: backend,
+				target: mode == 'development' ? backend : 'http://server:8000',
 				ws: false,
 				secure: false,
 			},
 		},
 	},
-})
+}))
