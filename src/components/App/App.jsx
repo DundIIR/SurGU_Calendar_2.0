8460f542b6117dedff83981852522a9c51f8ebfe
DundIR-PC <ruzin-de@yandex.ru>
2024-12-21 22:29:29 +0500

    Добавлен запрос на проверку роли пользователя, и начальная страница для админа

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes, Navigate } from 'react-router-dom'
-import { DefaultPage, HomePage } from '../../pages'
+import { DefaultPage, HomePage, AdminPage } from '../../pages'
 import { useEffect, useState } from 'react'
 import { useSession, useSessionContext, useSupabaseClient } from '@supabase/auth-helpers-react'
+import SurguCalendarAPI from '../../services/SurguCalendarAPI'
 
 const App = () => {
 	const session = useSession()
 	const { isLoading } = useSessionContext()
-
+	const [role, setRole] = useState(null)
+	const [isRoleLoading, setIsRoleLoading] = useState(true)
 	const [searches, setSearches] = useState([])
 
 	const updateSchedule = () => {
@@ -18,7 +20,27 @@ const App = () => {
 		updateSchedule()
 	}, [])
 
-	if (isLoading) {
+	useEffect(() => {
+		const validateUserRole = async () => {
+			if (session) {
+				try {
+					const surguCalendarAPI = new SurguCalendarAPI()
+					const data = await surguCalendarAPI.validateToken(session.access_token)
+					setRole(data.user.role)
+					console.log(data.user.role)
+				} catch (error) {
+					console.error('Ошибка валидации токена:', error)
+				} finally {
+					setIsRoleLoading(false)
+				}
+			} else {
+				setIsRoleLoading(false)
+			}
+		}
+		validateUserRole()
+	}, [session])
+
+	if (isLoading || isRoleLoading || role === null) {
 		return ''
 	}
 
@@ -28,7 +50,11 @@ const App = () => {
 				path="/"
 				element={
 					session ? (
-						<Navigate to="/home" />
+						role === 'Администратор' ? (
+							<Navigate to="/admin" />
+						) : (
+							<Navigate to="/home" />
+						)
 					) : (
 						<DefaultPage updateSchedule={updateSchedule} searches={searches} setSearches={setSearches} />
 					)
@@ -44,6 +70,7 @@ const App = () => {
 					)
 				}
 			/>
+			<Route path="/admin" element={session && role === 'Администратор' ? <AdminPage /> : <Navigate to="/home" />} />
 		</Routes>
 	)
 }
